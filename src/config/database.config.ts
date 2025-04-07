337b132d818d7261e7742aa41d0b6174ab26348b
Doseung Yang <doseung.dev@gmail.com>
2025-04-07 11:25:30 +0900

    fix: ValidationPipe 제거

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,9 +6,9 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [Post, Comment],
-  synchronize: true, // 개발 초기 단계에서는 true로 설정 가능, 프로덕션에서는 false 권장
+  synchronize: true,
   ssl: {
-    rejectUnauthorized: false, // Render PostgreSQL에 필요
+    rejectUnauthorized: false,
   },
   logging: false,
 };
